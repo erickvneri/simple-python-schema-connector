a3f770b5c548edc52c5cbe91f64cac1b20d90ab0
erickvneri <erickv@bluetrailsoft.com>
2020-08-06 19:59:54 -0500

    discovery query

--- util/schema_db/schema_db.py
+++ util/schema_db/schema_db.py
@@ -4,13 +4,13 @@
 # Access interaction type
 from stschema import SchemaDevice
 from dataclasses import dataclass
-import logging
+from datetime import datetime
+from sqlite3 import OperationalError, IntegrityError
 import sqlite3
+import logging
 import os
-
 # Logger config.
 logging.basicConfig(format='%(asctime)s - %(message)s')
-
 
 
 @dataclass
@@ -28,8 +28,10 @@ class SchemaDB:
         try:
             db = sqlite3.connect(self.DB_URL)
             self._create_all(db)
-        except sqlite3.OperationalError as e:
+        except (OperationalError, IntegrityError) as e:
             logging.warning('DATABASE ERROR - %s' % e)
+        else:
+            db.commit()
         finally:
             db.close()
 
@@ -39,19 +41,24 @@ class SchemaDB:
         self._create_devices_table(database)
         self._create_poll_table(database)
         self._create_user_table(database)
+        # Run mock inserts
+        self._user_mock_inserts(database)
+        self._token_mock_inserts(database)
+        self._devices_mock_inserts(database)
 
     @staticmethod
     def _create_callback_table(database: object) -> None:
         callback_table_query = \
             'CREATE TABLE IF NOT EXISTS ' + \
-            'CALLBACK_ARGS(' + \
-            'id INT AUTO_INCREMENT,' + \
+            'CALLBACK_INFO(' + \
+            'id INTEGER PRIMARY KEY,' + \
             'callback_url VARCHAR,' + \
             'oauth_url VARCHAR,' + \
             'code VARCHAR,' + \
             'client_id VARCHAR,' + \
             'client_secret VARCHAR,' + \
-            'PRIMARY KEY (id))'
+            'cb_access_token VARCHAR,'+ \
+            'cb_refresh_token VARCHAR)'
 
         cursor = database.cursor()
         cursor.execute(callback_table_query)
@@ -61,11 +68,12 @@ class SchemaDB:
         token_table_query = \
             'CREATE TABLE IF NOT EXISTS ' + \
             'TOKEN_INFO(' + \
-            'id INT AUTO_INCREMENT,' + \
+            'id INTEGER PRIMARY KEY,' + \
+            'user_id INT,' + \
             'access_token VARCHAR,' + \
             'refresh_token VARCHAR,' + \
             'last_update TIMESTAMP,' + \
-            'PRIMARY KEY (id))'
+            'FOREIGN KEY (user_id) REFERENCES USER_INFO(id))'
         cursor = database.cursor()
         cursor.execute(token_table_query)
 
@@ -74,12 +82,11 @@ class SchemaDB:
         users_table_query = \
             'CREATE TABLE IF NOT EXISTS ' + \
             'USER_INFO(' + \
-            'id INT AUTO_INCREMENT,' + \
+            'id INTEGER PRIMARY KEY UNIQUE,' + \
             'username VARCHAR,' + \
             'password VARCHAR,' + \
             'last_login TIMESTAMP,' + \
-            'created_date TIMESTAMP,' + \
-            'PRIMARY KEY (id))'
+            'created_date TIMESTAMP)'
         cursor = database.cursor()
         cursor.execute(users_table_query)
 
@@ -92,8 +99,8 @@ class SchemaDB:
             'user_id INT,' + \
             'label VARCHAR,' + \
             'device_handler VARCHAR,' + \
+            'mn VARCHAR,' + \
             'model VARCHAR,' + \
-            'room VARCHAR,' + \
             'PRIMARY KEY (id),' + \
             'FOREIGN KEY (user_id) REFERENCES USER_INFO(id))'
         cursor = database.cursor()
@@ -104,13 +111,87 @@ class SchemaDB:
         poll_table_query = \
             'CREATE TABLE IF NOT EXISTS ' + \
             'POLL_INFO(' + \
-            'id INT AUTO_INCREMENT,' + \
+            'id INTEGER PRIMARY KEY,' + \
             'device_id VARCHAR,' + \
             'poll_date TIMESTAMP,' + \
             'capability VARCHAR,' + \
             'attribute VARCHAR,' + \
             'value VARCHAR,' + \
-            'PRIMARY KEY (id),' + \
             'FOREIGN KEY (device_id) REFERENCES DEVICE_INFO(id))'
         cursor = database.cursor()
         cursor.execute(poll_table_query)
+
+    def prov_user_devices(self, user_tkn):
+        """
+        prov_user_devices is invoked while
+        gathering response data for Schema
+        Discovery Requests.
+        """
+        try:
+            db = sqlite3.connect(self.DB_URL)
+        except OperationalError as e:
+            logging.warning(e)
+        else:
+            cursor = db.cursor()
+            return self._prov_user_devices(cursor, user_tkn)
+
+    @staticmethod
+    def _prov_user_devices(*info):
+        cursor, param = info
+        # From auth_token received, get
+        # user id and return respective
+        # devices.
+        user_id_query = \
+            'SELECT user_id FROM USER_INFO ' + \
+            'LEFT JOIN TOKEN_INFO ' + \
+            'ON USER_INFO.id=TOKEN_INFO.user_id ' + \
+            'WHERE TOKEN_INFO.access_token="%s"' % param
+        devices_query = \
+            'SELECT * FROM DEVICE_INFO ' + \
+            'WHERE user_id=(%s)' % user_id_query
+        data = cursor.execute(devices_query)
+        return data.fetchall()
+
+
+#############################################################
+#############################################################
+############# MOCK DATA FOR TESTING PURPOSES ################
+#############################################################
+#############################################################
+    @staticmethod
+    def _user_mock_inserts(database):
+        cursor = database.cursor()
+        cursor.execute('DELETE FROM USER_INFO')
+        for i in range(1,200):
+            q_user = \
+                'INSERT INTO USER_INFO' + \
+                '(username, password,last_login,created_date)' + \
+                'VALUES ' + \
+                '("USER_%s","PASS","%s", "%s")' % (i,datetime.now(),datetime.now())
+            cursor.execute(q_user)
+
+    @staticmethod
+    def _token_mock_inserts(database):
+        cursor = database.cursor()
+        cursor.execute('DELETE FROM TOKEN_INFO')
+        for i in range(1, 200)    :
+            q_tkn = \
+                'INSERT INTO TOKEN_INFO' +\
+                '(user_id,access_token,refresh_token,last_update)' + \
+                'VALUES ' + \
+                '(%s,"access_token_%s","refresh_token_%s","%s")' % (i,i,i,datetime.now())
+            cursor.execute(q_tkn)
+
+    @staticmethod
+    def _devices_mock_inserts(database):
+        from random import randint
+        cursor = database.cursor()
+        cursor.execute('DELETE FROM DEVICE_INFO')
+        for i in range(1, 1000):
+            q_device = \
+                'INSERT INTO DEVICE_INFO' + \
+                '(id,user_id,label,device_handler,mn,model) ' + \
+                'VALUES ' + \
+                '("x%s", %i, "device_%s","c2c-switch","MNMN","MODEL")' % (i,randint(1,200),i)
+            cursor.execute(q_device)
+
