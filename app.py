7a093bb8754f7834b70b819ac01b1f7f00eece17
erickvneri <erickv@bluetrailsoft.com>
2020-08-18 23:02:03 -0500

    patch

--- app.py
+++ app.py
@@ -6,8 +6,3 @@ db = MainDB()
 
 if __name__ == '__main__':
     db.init()
-
-    from lib.resources import Connector
-    from pprint import pprint
-    c = Connector()
-    pprint(c.discovery_handler('asdfasd','access_token_925'))
