cc0f81cb146abffb0d69b2e21b7e41edff975136
erickvneri <erickv@bluetrailsoft.com>
2020-09-28 00:40:37 -0500

    simplified data usage

--- lib/oauth/data/user_information.py
+++ lib/oauth/data/user_information.py
@@ -2,18 +2,20 @@ import jwt
 import logging
 import pickle
 import os
+import secrets
 from hashlib import md5
 from datetime import datetime
-from lib.oauth.oauth_config import (USER_INFO_PATH,
-                                    SECRET,
-                                    ALGORITHM,
-                                    TOKEN_TYPE,
-                                    CLIENT_ID)
+from lib.oauth.oauth_config import (
+    USER_INFO_PATH,
+    SECRET,
+    ALGORITHM,
+    TOKEN_TYPE,
+    CLIENT_ID,
+)
 
 
 # LOGGING CONFIG
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 
 class User:
@@ -27,97 +29,141 @@ class User:
 
 class BearerToken:
     def __init__(self, **kwargs):
-        self.access_token = kwargs.get('access_token')
-        self.refresh_token = kwargs.get('refresh_token')
-        self.code = kwargs.get('code')
-        self.expires_in = kwargs.get('expires_in')
-        self.token_type = kwargs.get('token_type')
+        self.access_token = kwargs.get("access_token")
+        self.refresh_token = kwargs.get("refresh_token")
+        self.code = kwargs.get("code")
+        self.expires_in = kwargs.get("expires_in")
+        self.token_type = kwargs.get("token_type")
 
 
-class UserInformation():
-    # def __init__(self):
+class UserInformation:
     basedir = os.path.abspath(os.path.dirname(__file__))
     user_info_path = USER_INFO_PATH
 
     def grant_user_access(self, *data):
         email, password = data
         new = User(email, password, str(datetime.now()))
-        new.user_id = md5(str(new.__dict__).encode('utf-8')).hexdigest()
-        self._save_record(new.__dict__) # saving record at user_information.p
+        new.user_id = md5(str(new.__dict__).encode("utf-8")).hexdigest()
+        self._save_user_record(new.__dict__)  # saving record at user_information.p
         return new.user_id
 
     def create_bearer_token(self, **data):
-        import secrets
         # Create BearerToken instance.
         # Returns __dict__ of class instance
         # with JWT tokens.
         #
         # Data to encode
-        access_jwt_data = dict(devices=data.get('devices'))
-        refresh_jwt_data = dict(user_id=data.get('user_id'))
+        access_jwt_data = data.get("devices")
+        access_jwt_data['timestamp'] = str(datetime.now())
+        refresh_jwt_data = dict(user_id=data.get("user_id"), timestamp=str(datetime.now()))
         # Create JSON Web Tokens
-        acc_token = jwt.encode(access_jwt_data, SECRET, algorithm=ALGORITHM).decode('utf-8')
-        ref_token = jwt.encode(refresh_jwt_data, SECRET, algorithm=ALGORITHM).decode('utf-8')
+        acc_token = jwt.encode(access_jwt_data, SECRET, ALGORITHM).decode("utf-8")
+        ref_token = jwt.encode(refresh_jwt_data, SECRET, ALGORITHM).decode("utf-8")
         expires_in = 3600
         code = secrets.token_hex(20)
         # BearerToken instance
-        bearer_token = BearerToken(access_token=acc_token,
-                                   refresh_token=ref_token,
-                                   code=code,
-                                   expires_in=expires_in,
-                                   token_type=TOKEN_TYPE)
+        bearer_token = BearerToken(
+            access_token=acc_token,
+            refresh_token=ref_token,
+            code=code,
+            expires_in=expires_in,
+            token_type=TOKEN_TYPE,
+        )
         # Update user data to include
         # set of JWT Tokens.
-        self._save_bearer_token(user_id=data.get('user_id'),
-                                bearer_token=bearer_token.__dict__)
+        self._save_bearer_token(
+            user_id=data.get("user_id"), bearer_token=bearer_token.__dict__
+        )
 
-        logging.info('authorization code generated.')
+        logging.info("authorization code generated.")
         return code
 
     def _save_bearer_token(self, **data) -> None:
         file_path = self.basedir + self.user_info_path
-        # Read file step
-        _file = open(file_path, 'rb')
+        # Read and update record.
+        _file = open(file_path, "rb")
         info = pickle.load(_file)
-        # Filter user
-        user = list(filter(lambda u: u['user_id'] == data['user_id'], info))
-        user[0]['bearer_token'] = data['bearer_token']
+        info[0]["bearer_token"] = data["bearer_token"]
 
-        # Write file step
-        _file = open(file_path, 'wb')
+        # Write changes.
+        _file = open(file_path, "wb")
         pickle.dump(info, _file)
         _file.close()
-        logging.info('bearer token saved.')
+        logging.info("bearer token saved.")
 
-    def get_access_token(self, code: str) -> dict: # FIXME: Revoke code
+    def get_access_token(self, code: str) -> dict:
         # Return tokens exchange for
         # POST Http Request at /token.
-        with open(self.basedir + self.user_info_path, 'rb') as _file:
+        with open(self.basedir + self.user_info_path, "rb") as _file:
             info = pickle.load(_file)
-            user_info = list(filter(lambda user: user['bearer_token']['code'] == code[0], info))
-        return user_info[0]['bearer_token']
+            info[0]["bearer_token"].pop("code")
+            self._revoke_oauth_code(code)
+        return info[0]["bearer_token"]
 
     def refresh_token(self, refresh_token: str) -> dict:
         # Refresh user tokens.
-        pass
+        file_path = self.basedir + self.user_info_path
+        _file = open(file_path, 'rb')
+        info = pickle.load(_file)
+        token_data = info[0]['bearer_token']
 
-    def _save_record(self, user_data):
+        # Validate if refresh token.
+        # if not refresh_token == token_data['refresh_token']:
+            # return False
+
+        # Decode refresh token
+        decoded_refresh = jwt.decode(refresh_token, SECRET, ALGORITHM)
+        ref_token_data = dict(user_id=decoded_refresh['user_id'],timestamp=str(datetime.now()))
+        # Access token data
+        cur_access_token = token_data['access_token']
+        acc_token_data = jwt.decode(cur_access_token, SECRET, ALGORITHM)
+        acc_token_data['timestamp'] = str(datetime.now())
+
+        # Redefine tokens
+        token_data['access_token'] = jwt.encode(acc_token_data, SECRET, ALGORITHM).decode('utf-8')
+        token_data['refresh_token'] = jwt.encode(ref_token_data, SECRET, ALGORITHM).decode("utf-8")
+
+        # Update file.
+        _file = open(file_path, 'wb')
+        pickle.dump(info, _file)
+        _file.close()
+        logging.info('access token refreshed')
+        token_data.pop('code')
+        return token_data
+
+    def _revoke_oauth_code(self, code):
+        # Once OAuth Code has been
+        # exchanged by an access_token,
+        # it will be revoked.
+        #
+        # Read and set to None current code
+        file_path = self.basedir + self.user_info_path
+        _file = open(file_path, "rb")
+        info = pickle.load(_file)
+        info[0]["bearer_token"]["code"] = None
+        # Update values
+        _file = open(file_path, "wb")
+        pickle.dump(info, _file)
+        _file.close()
+        logging.info("OAuth code revoked successfully.")
+
+    def _save_user_record(self, user_data):
         # Read file step
         file_path = self.basedir + self.user_info_path
         try:
-            _file = open(file_path, 'rb')
+            _file = open(file_path, "rb")
             info = pickle.load(_file)
-        except EOFError:
-            # If file totally empty,
-            # then insert [].
-            _file = open(file_path, 'wb')
+        except (EOFError, FileNotFoundError):
+            # When file is empty or doesn't exists.
+            _file = open(file_path, "wb")
             pickle.dump([], _file)
             _file.close()
-            return self._save_record(user_data)
+            return self._save_user_record(user_data)
         else:
-            info = [] if not isinstance(info, list) else info
+            info = []
             info.append(user_data)
-            _file = open(file_path, 'wb')
+            # Write data.
+            _file = open(file_path, "wb")
             pickle.dump(info, _file)
             _file.close()
-            logging.info('new user information stored')
+            logging.info("new user information stored")
