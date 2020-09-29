88fa1951f258bf31c8aa92adb654bf4e1b8c20b1
erickvneri <erickv@bluetrailsoft.com>
2020-09-29 00:39:06 -0500

    pickle data handler

--- lib/oauth/data/user_information.py
+++ lib/oauth/data/user_information.py
@@ -1,16 +1,32 @@
-import jwt
+"""
+To simplify the storage of user and
+token information, this module will
+serve as database saving data in
+binary form using the pickle built-in
+module.
+
+In addition, to avoid massive data
+storage, the <user_info.b> file
+will store only one record, therefore,
+data will be overwritten at every
+login attempt, e.g.:
+  [{
+      "email": ...,
+      "password"...,
+      "bearer_token": {...},
+  }].
+"""
+from datetime import datetime
 import logging
 import pickle
 import os
 import secrets
-from hashlib import md5
-from datetime import datetime
+import jwt
 from lib.oauth.oauth_config import (
     USER_INFO_PATH,
     SECRET,
     ALGORITHM,
-    TOKEN_TYPE,
-    CLIENT_ID,
+    TOKEN_TYPE
 )
 
 
@@ -23,29 +39,16 @@ class User:
         self.email = email
         self.password = password
         self.last_login = last_login
-        self.user_id = None
         self.bearer_token = None
-
-
-class BearerToken:
-    def __init__(self, **kwargs):
-        self.access_token = kwargs.get("access_token")
-        self.refresh_token = kwargs.get("refresh_token")
-        self.code = kwargs.get("code")
-        self.expires_in = kwargs.get("expires_in")
-        self.token_type = kwargs.get("token_type")
-
 
 class UserInformation:
     basedir = os.path.abspath(os.path.dirname(__file__))
     user_info_path = USER_INFO_PATH
 
-    def grant_user_access(self, *data):
+    def grant_user_access(self, *data) -> None:
         email, password = data
         new = User(email, password, str(datetime.now()))
-        new.user_id = md5(str(new.__dict__).encode("utf-8")).hexdigest()
         self._save_user_record(new.__dict__)  # saving record at user_information.p
-        return new.user_id
 
     def create_bearer_token(self, **data):
         # Create BearerToken instance.
@@ -55,25 +58,23 @@ class UserInformation:
         # Data to encode
         access_jwt_data = data.get("devices")
         access_jwt_data['timestamp'] = str(datetime.now())
-        refresh_jwt_data = dict(user_id=data.get("user_id"), timestamp=str(datetime.now()))
+        refresh_jwt_data = dict(timestamp=str(datetime.now()))
         # Create JSON Web Tokens
         acc_token = jwt.encode(access_jwt_data, SECRET, ALGORITHM).decode("utf-8")
         ref_token = jwt.encode(refresh_jwt_data, SECRET, ALGORITHM).decode("utf-8")
         expires_in = 3600
         code = secrets.token_hex(20)
-        # BearerToken instance
-        bearer_token = BearerToken(
+        # BearerToken dictionary
+        bearer_token = dict(
             access_token=acc_token,
             refresh_token=ref_token,
             code=code,
             expires_in=expires_in,
-            token_type=TOKEN_TYPE,
+            token_type=TOKEN_TYPE
         )
         # Update user data to include
         # set of JWT Tokens.
-        self._save_bearer_token(
-            user_id=data.get("user_id"), bearer_token=bearer_token.__dict__
-        )
+        self._save_bearer_token(bearer_token=bearer_token)
 
         logging.info("authorization code generated.")
         return code
@@ -96,8 +97,12 @@ class UserInformation:
         # POST Http Request at /token.
         with open(self.basedir + self.user_info_path, "rb") as _file:
             info = pickle.load(_file)
+            # Validate code
+            if not code == info[0]['bearer_token']['code']:
+                return None
+            # Return bearer token
             info[0]["bearer_token"].pop("code")
-            self._revoke_oauth_code(code)
+            self._revoke_oauth_code()
         return info[0]["bearer_token"]
 
     def refresh_token(self, refresh_token: str) -> dict:
@@ -108,12 +113,11 @@ class UserInformation:
         token_data = info[0]['bearer_token']
 
         # Validate if refresh token.
-        # if not refresh_token == token_data['refresh_token']:
-            # return False
+        if not refresh_token == token_data['refresh_token']:
+            return None
 
-        # Decode refresh token
-        decoded_refresh = jwt.decode(refresh_token, SECRET, ALGORITHM)
-        ref_token_data = dict(user_id=decoded_refresh['user_id'],timestamp=str(datetime.now()))
+        # Refresh Token data
+        ref_token_data = dict(timestamp=str(datetime.now()))
         # Access token data
         cur_access_token = token_data['access_token']
         acc_token_data = jwt.decode(cur_access_token, SECRET, ALGORITHM)
@@ -131,7 +135,7 @@ class UserInformation:
         token_data.pop('code')
         return token_data
 
-    def _revoke_oauth_code(self, code):
+    def _revoke_oauth_code(self) -> None:
         # Once OAuth Code has been
         # exchanged by an access_token,
         # it will be revoked.
@@ -147,7 +151,7 @@ class UserInformation:
         _file.close()
         logging.info("OAuth code revoked successfully.")
 
-    def _save_user_record(self, user_data):
+    def _save_user_record(self, user_data) -> None:
         # Read file step
         file_path = self.basedir + self.user_info_path
         try:
