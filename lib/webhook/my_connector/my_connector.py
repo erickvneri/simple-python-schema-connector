0a5f714c907d9b9242a94f7942fea3b138b68675
erickvneri <erickv@bluetrailsoft.com>
2020-09-30 00:23:51 -0500

    discovery - state refresh flow

--- lib/webhook/my_connector/my_connector.py
+++ lib/webhook/my_connector/my_connector.py
@@ -1,6 +1,9 @@
-from stschema import SchemaConnector
+import jwt
+from stschema import SchemaConnector, SchemaDevice
+from lib.webhook.webhook_config import ALGORITHM, SECRET
+from lib.webhook.data import DeviceInformation
 
-class MyConnector(SchemaConnector):
+class MyConnector(SchemaConnector, DeviceInformation):
     def __init__(self, *opts):
         SchemaConnector.__init__(self, enable_logger=True)
 
@@ -12,7 +15,27 @@ class MyConnector(SchemaConnector):
         # as a list argument to discovery_response
         # built-in method.
         declared_devices = []
-        return self.discovery_response(declared_devices, request_id)
+
+        # Decode access token and collect raw device info
+        token_info = jwt.decode(access_token, SECRET, ALGORITHM)
+        device_info = super().get_device_info(token_info['devices'])
+
+        # Create SchemaDevice instances
+        for device in device_info:
+            # Discovery information
+            instance = SchemaDevice(device['unique_id'],        # externalDeviceId
+                                    device['label'],            # friendlyName
+                                    device['device_handler'],   # deviceHandlerType
+                                    device['unique_id'])        # deviceUniqueId
+            # Manufacturer Information
+            instance.set_mn(device['mn']['mn'],
+                            device['mn']['model'])
+
+            # Collect instance into predefined list
+            declared_devices.append(instance)
+
+        # Return Discovery Response
+        return super().discovery_response(declared_devices, request_id)
 
     def state_refresh_handler(self, devices, request_id, access_token):
         # The state_refresh_handler gives access to
@@ -21,6 +44,24 @@ class MyConnector(SchemaConnector):
         # must be passed as response to the
         # state_refresh_response built-in method.
         filtered_devices = []
+        device_ids = list(map(lambda device: device['externalDeviceId'], devices))
+
+        # Collect raw device state info
+        device_info = super().get_states(device_ids)
+
+        # Create SchemaDevice instances
+        for device in device_info:
+            instance = SchemaDevice(device['unique_id'])
+            for state in device['states']:
+                instance.set_state(state.get('capability'),
+                                   state.get('attribute'),
+                                   state.get('value'),
+                                   state.get('unit'),
+                                   state.get('component'))
+
+            filtered_devices.append(instance)
+
+        # Return State Refresh Response
         return self.state_refresh_response(filtered_devices, request_id)
 
     def command_handler(self, devices, request_id, access_token):
