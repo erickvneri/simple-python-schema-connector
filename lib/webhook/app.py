6144f75314ebad180acaf0c7e88bcb1690ec4a8a
erickvneri <erickv@bluetrailsoft.com>
2020-09-30 00:23:23 -0500

    typo

--- lib/webhook/app.py
+++ lib/webhook/app.py
@@ -1,6 +1,10 @@
+"""
+Docstring here...
+"""
 import json
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler
+from lib.webhook.my_connector import MyConnector
 from lib.webhook.webhook_config import (ADDRESS,
                                         PORT,
                                         ENCODING,
@@ -8,60 +12,53 @@ from lib.webhook.webhook_config import (ADDRESS,
 
 
 class Webhook(BaseHTTPRequestHandler):
-    """
-    Comments here...
-    """
     def do_POST(self):
         # POST Http Request Handler
         if self.path != WEBHOOK_ENDPOINT:
             # Endpoint not supported
-            return self.send_error(HTTPStatus.BAD_REQUEST)
+            self.send_error(HTTPStatus.BAD_REQUEST)
 
         content_length = int(self.headers['Content-Length'])
         req_body = self.rfile.read(content_length).decode(ENCODING)
         # Parse JSON string
         json_data = json.loads(req_body)
-        return self._authorize_request(json_data)
+        self._authorize_request(json_data)
 
-    def _authorize_request(self, data):
-        authentication = data.get('authentication')
+    def _authorize_request(self, json_data):
+        authentication = json_data.get('authentication')
         if not authentication:
-            return self.send_error(HTTPStatus.UNAUTHORIZED)
+            self.send_error(HTTPStatus.UNAUTHORIZED)
         else:
             bearer_token = authentication.get('token')
             token_type = authentication.get('tokenType')
             if not bearer_token:
-                return self.send_error(HTTPStatus.UNAUTHORIZED)
-            elif token_type and tokenType != 'Bearer':
-                return self.send_error(HTTPStatus.UNAUTHORIZED)
+                self.send_error(HTTPStatus.UNAUTHORIZED)
+            elif token_type and token_type != 'Bearer':
+                self.send_error(HTTPStatus.UNAUTHORIZED)
             else:
-                if not self._authorize_bearer_token(bearer_token):
-                    return self.send_error(HTTPStatus.UNAUTHORIZED)
-                else:  # FIXME: Extra indentation could be deleted
-                    user_data = self._decode_bearer_token(bearer_token)['devices']
-                    return self._schema_connector_route(data, user_data)
+                # if not self._authorize_bearer_token(bearer_token):
+                    # self.send_error(HTTPStatus.UNAUTHORIZED)
+                # Proceed to send interaction types
+                # to the SchemaConnector instance.
+                self._schema_connector_route(json_data)
 
     def _authorize_bearer_token(self):
         # Authorize Bearer Token
         # with OAuth Instance.
         raise NotImplementedError('Not implemented yet.')
 
-    def _decode_bearer_token(self):
-        # Decode Bearer Token to access
-        # device selection from OAuth
-        # user config flow.
-        raise NotImplementedError('Not implemented yet.')
-
-    def _schema_connector_route(self, *args):
+    def _schema_connector_route(self, json_data):
         # Call to interacion_handler from
         # SchemaConnector Instance.
-        raise NotImplementedError('Not implemented yet.')
+        my_connector = MyConnector()
+        response_data = my_connector.interaction_handler(json_data)
+        self._success_response(response_data)
 
-    def _success_response(self, data):
+    def _success_response(self, json_data):
         # Http Response envelope config
         self.send_response(HTTPStatus.OK)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
         # Http Response body config
-        json_response = json.dumps(data).encode(ENCODING)
+        json_response = json.dumps(json_data).encode(ENCODING)
         self.wfile.write(json_response)
